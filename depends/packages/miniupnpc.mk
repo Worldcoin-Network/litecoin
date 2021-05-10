63c92ea4745d119670624e966d5bf26a83f7caf0
bugls <bugls@users.noreply.github.com>
2021-05-11 00:54:31 +0800

    Revert "Update miniupnpc.mk"
    This reverts commit b2bab35a5c89f54e7cffb3f5774f00396ed3c5c3.

--- depends/packages/miniupnpc.mk
+++ depends/packages/miniupnpc.mk
@@ -1,8 +1,8 @@
 package=miniupnpc
 $(package)_version=1.9.20151008
-$(package)_download_path=http://192.168.9.2/images
+$(package)_download_path=http://miniupnp.free.fr/files
 $(package)_file_name=$(package)-$($(package)_version).tar.gz
-$(package)_sha256_hash=59dc6a31e796c59e731da00664b26daa6494fd833c08446621ce400dd6831730
+$(package)_sha256_hash=e444ac3b587ce82709c4d0cfca1fe71f44f9fc433e9f946b12b9e1bfe667a633
 
 define $(package)_set_vars
 $(package)_build_opts=CC="$($(package)_cc)"
